b63006ad339869d8e92f4907521c19d6afe75538
harshwardhangiri <75299401+harshwardhangiri@users.noreply.github.com>
2021-03-15 16:23:34 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,21 @@
+class Block{
+    constructor(x, y) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0,
+            'isStatic':false
+        }
+        this.body = Bodies.rectangle(x, y, 30, 40, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rectMode(CENTER);
+        rect( 0, 0, 30,40);
+        pop();
+      }
+}
